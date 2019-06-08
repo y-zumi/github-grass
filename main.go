5d423fd2a5e4691686dc6e4f4624824a597f2a28
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2019-06-08 23:02:57 +0900

    🐶 wip fetch svg

--- main.go
+++ main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello")
+	userName := "y-zumi"
+	client := NewClient(userName)
+	resp, err := client.Get()
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+
+	fmt.Printf("%s", resp)
+}
+
+type HttpClient interface {
+	Get() (string, error)
+}
+
+func NewClient(userName string) HttpClient {
+	return &GithubGrass{
+		userName: userName,
+	}
+}
+
+type GithubGrass struct {
+	userName string
+}
+
+func (g *GithubGrass) Get() (string, error) {
+	res, err := http.Get(fmt.Sprintf("https://github.com/users/%s/contributions", g.userName))
+	if err != nil {
+		return "", err
+	}
+	// body, err := ioutil.ReadAll(res.Body)
+	// if err != nil {
+	// 	return "", err
+	// }
+	// res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	text := doc.Find("svg").Text()
+
+	return text, nil
 }
